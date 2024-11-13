f065e484076c2b576398d19b801446576ccba87e
rnsharma <rnsharma@gmail.com>
2024-11-12 20:11:50 -0800

    fixes

--- main.py
+++ main.py
@@ -71,5 +71,5 @@ def extract_invoice():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
